3402f400da5e7a97fa346038b9704b9c9edf08e2
Amaan <amaan14999@gmail.com>
2024-04-13 21:37:59 +0530

    contact page api response: 200

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -2,26 +2,14 @@ import zod from "zod";
 import stripIndent from "strip-indent";
 import { Resend } from "resend";
 import EmailTemplate from "@/email-templates/EmailTemplate";
-
 const contactFormValidator = zod.object({
   name: zod
-    .string({
-      required_error: "Name is required",
-      invalid_type_error: "Name must be a string",
-    })
+    .string()
     .min(3, { message: "Name must be at least 3 characters long" })
     .max(50, { message: "Name must be less than 50 characters long" }),
-  email: zod
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Invalid Email",
-    })
-    .email({ message: "Invalid email" }),
+  email: zod.string().email({ message: "Invalid email" }),
   message: zod
-    .string({
-      required_error: "Message is required",
-      invalid_type_error: "Invalid message",
-    })
+    .string()
     .min(3, { message: "Message must be at least 3 characters long" })
     .max(500, { message: "Message must be less than 500 characters long" }),
 });
@@ -43,16 +31,14 @@ async function sendContactFormEmail(name, email, message) {
   });
 }
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const data = await req.json();
+export const POST = async (req, res) => {
+  const data = await req.json();
 
+  try {
     const parseResult = contactFormValidator.safeParse(data);
     if (!parseResult.success) {
       return res.status(400).json(parseResult.error.flatten());
-    }
-
-    try {
+    } else {
       const emailResponse = await sendContactFormEmail(
         parseResult.data.name,
         parseResult.data.email,
@@ -63,19 +49,15 @@ export default async function handler(req, res) {
       } else {
         return res.status(200).json({ message: "Email sent successfully" });
       }
-    } catch (error) {
-      return res.status(500).json({ message: "Internal Server Error" });
     }
-  } else if (req.method === "OPTIONS") {
-    // Support for CORS
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    };
-    res.headers(headers);
-    return res.status(200).end();
-  } else {
-    return res.status(405).end(); // Method Not Allowed
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
+
+export const OPTIONS = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  return res.status(200).end();
+};
