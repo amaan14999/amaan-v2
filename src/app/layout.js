2a84bd5a66aab570956811ce75dfff8ffd251492
Amaan <amaan14999@gmail.com>
2024-04-17 18:18:06 +0530

    favicon fix (#2)
    Signed-off-by: Amaan <amaan14999@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,7 +68,7 @@ export const metadata = {
   description:
     "A Full-stack developer building fast, accessible, and user-centric websites and apps",
   icons: {
-    shortcut: "/Logo.ico",
+    shortcut: "/favicon.ico",
     apple: "/Logo.png",
     icon: "/Logo.png",
   },
